58727d48d1f1fbb5ee25c5f898797edd517b629d
Catherine Carbert <catherine.carbert@ucalgary.ca>
2025-01-19 21:24:27 -0700

    made changs

--- src/main.js
+++ src/main.js
@@ -37,7 +37,7 @@ document.querySelector("#app").innerHTML = `
               )
               .join("")}
           </div>
-          <h4 id="description">This Cadavre tackles the question of what it means to be a human. The collage highlights some of humanity's most significant creations, events, and scenes, aiming to represents what makes up a human. While each individual is a miracle of life themselves, the meaning of a human is to be everything we have ever seen, done, created, whether good or bad.</h4>
+          <h4 id="description">This Cadavre tackles the question of what it means to be a human. The collage highlights some of humanity's most significant creations, events, and scenes, aiming to represent what makes up a human. While each individual is a miracle of life themselves, the meaning of a human is to be everything we have ever seen, done, created, whether good or bad.</h4>
         </div>
       </div>
 
